0b5b0fc887c65a95bf81be82a6bd4835994da804
Rafael <rafaelramosdeandrade20@gmail.com>
2023-11-08 10:49:28 -0300

    Writers and readers implementation using semaphore

--- semaphore/main.c
+++ semaphore/main.c
@@ -1,136 +1,139 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdio.h>
-#include <string.h>
-#include <unistd.h>
 #include <time.h>
 
 #include <pthread.h>
 #include <semaphore.h>
-#include <fcntl.h>
 
 #define MAX 10
-#define CONSUMER 10
+#define READERS 1
 
 unsigned count = 0;
 
-struct{
+struct buffer{
     double *data;
     unsigned size;
-    unsigned cur;
-    sem_t* sem_addr;
-}typedef buffer;
+    unsigned tail;
+    unsigned head;
+    
+    int readers;
+    sem_t sem;
+    pthread_mutex_t lock;
+};
 
-buffer * buffer_create(){
+struct buffer * buffer_create ()
+{
 
-    buffer *b = malloc(sizeof(buffer));
-   
+    struct buffer *b = malloc(sizeof(struct buffer));
+
+    // init buffer variables
     b->data = malloc(sizeof(double) * MAX);
     b->size = MAX;
-    b->cur = 0;
+    b->readers = 0;
+
+    //Init semaphore
+    sem_init(&b->sem, 0, 1);
+
     
     return b;
 }
 
-void * produce(){
-    
-    double *c = malloc(sizeof(double));
-    unsigned n = rand() % 99999999;
+void read_data (struct buffer *b)
+{
+    double c = b->data[rand() % b->size];
 
-    for(int i = 0; i<n; i++){
-            double a = 34.123412;
-            double b = 3.22412345;
-
-            *c = a / b * i;
+    for (int i = 0; i < 9200; i++) {
+        double a = 34.123412;
+        double b = 34.123412;
+        c = a * b * i;
     }
-
-    void *obj = c;
-    
-    return obj;
 }
 
-void * consume(void *obj){
-    double *c = obj;
-    
-    unsigned n = rand() % 99999999;
 
-    for(int i = 0; i<n; i++){
-            double a = 34.123412;
-            double b = 3.22412345;
-
-            *c = a * b * i;
-    }    
-}
-
-void push(void *obj, buffer *b){
-    double *obj_p = obj;
-    b->data[b->cur] = *obj_p;
-    b->cur++;
-}
-
-void * pop(buffer *b){
-    void *obj;
-    
-    obj = &b->data[b->cur-1];
-    b->cur--;
-
-    return obj;
-}
-
-void * writer(void *b){
-    buffer *buf = b;
+void * reader(void *buf)
+{
+    struct buffer *b = buf;
 
     while(true){
+        pthread_mutex_lock(&b->lock);
 
-        if(buf->cur > 0){
-            void *obj = pop(b);  
-            printf("consumer\n");
-            consume(obj);
-        }else{
-        }
+        b->readers++;
+        if (b->readers == 1)
+            sem_wait(&b->sem);
 
+        pthread_mutex_unlock(&b->lock);
+
+        read_data(b);
+
+        pthread_mutex_lock(&b->lock);
+
+        printf("R");
+
+        b->readers--;
+        if (b->readers == 0)
+            sem_post(&b->sem);
+        
+        pthread_mutex_unlock(&b->lock);
     }
 }
 
-void * producer(void *b){
-    buffer *buf = b;
-    while(true){
+void write_data (struct buffer *b)
+{
+    double c = rand();
 
-        if(buf->cur < buf->size){
-            void *obj = produce();
-            printf("producer\n");
-            push(obj, b);
-        }else{
-        }
+    for (int i = 0; i < 9200; i++) {
 
+        double a = 34.123412;
+        double b = 3.22412345;
+
+        c = a / b * i;
     }
+
+    b->data[rand() % b->size] = c;
+}
+
+
+void * writer (void *buf)
+{
+    struct buffer *b = buf;
+
+    while (true) {
+        sem_wait(&b->sem);
+        write_data(b);
+        printf("W");
+        sem_post(&b->sem);
+    }
+
 }
 
 int main(){
 
     srand(time(NULL));
+   
 
     //Alloc memory to the buffer.
-    buffer *b = buffer_create();
-    
-    //Create semaphore
-    b->sem_addr = sem_open("sem", O_CREAT);
+    struct buffer *b = buffer_create();
 
     //Steps to create the threads-----.
-    pthread_t consumer_id, producer_id;
+    pthread_t writer_id, reader_id[READERS];
     
     pthread_attr_t attr;
     
     pthread_attr_init(&attr);
 
-    pthread_create (&producer_id, &attr, &producer, b);
-    pthread_create (&consumer_id, &attr, &consumer, b);
+    pthread_create (&writer_id, &attr, &writer, b);
+
+    for(int i = 0; i < READERS; i++)
+        pthread_create (&reader_id[i], &attr, &reader, b);
 
     //---------------------------------
     
     //Join threads
-    pthread_join (consumer_id, NULL);
-    pthread_join (producer_id, NULL);
+    for(int i = 0; i < READERS; i++)
+        pthread_join (reader_id[i], NULL);
+
+    pthread_join (writer_id, NULL);
 
 }
 
